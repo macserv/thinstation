9abf057aa0cd01f47117f4eff1b9c92c04b33e26
Matthew Judy <matt+github.thinstation@nibfile.com>
2019-03-20 15:13:36 -0400

    Update usage output, and tweak boot-disk-making script

--- ts/build/jpmc/bin/mktsubstick.zsh
+++ ts/build/jpmc/bin/mktsubstick.zsh
@@ -28,7 +28,7 @@ zmodload zsh/zutil
 #    is created on your drive during this process.
 #    Default: "THINSTATION"
 #
-function mktsusbstick # --ts-repo <repo_path> --disk-id <disk_id> --new-volume-name [volume_name]
+function mktsusbstick # --mbr-path </usr/lib/syslinux/mbr/mbr.bin> --boot-dir-path </media/ubuntu/THINSTATION/boot> --disk-id <usb-SanDisk_Ultra_USB_3.0_UUID-0:0> --new-volume-name [THINSTATION]
 {
     zparseopts -A MKTSUSBSTICK_OPTIONS  \
         -mbr-path:                      \
@@ -36,9 +36,9 @@ function mktsusbstick # --ts-repo <repo_path> --disk-id <disk_id> --new-volume-n
         -disk-id:                       \
         -new-volume-name::
     
-    [[ -v MKTSUSBSTICK_OPTIONS[--mbr-path] ]]      || { echo ; echo '[ERROR]  Missing argument: --mbr-path.' ; return 1 }
-    [[ -v MKTSUSBSTICK_OPTIONS[--boot-dir-path] ]] || { echo ; echo '[ERROR]  Missing argument: --boot-dir-path.' ; return 2 }
-    [[ -v MKTSUSBSTICK_OPTIONS[--disk-id] ]]       || { echo ; echo '[ERROR]  Missing argument: --disk-id.' ; return 3 }
+    [[ -v MKTSUSBSTICK_OPTIONS[--mbr-path]      ]] || { echo ; echo '[ERROR]  Missing argument: --mbr-path.'      ; return 1 ; }
+    [[ -v MKTSUSBSTICK_OPTIONS[--boot-dir-path] ]] || { echo ; echo '[ERROR]  Missing argument: --boot-dir-path.' ; return 2 ; }
+    [[ -v MKTSUSBSTICK_OPTIONS[--disk-id]       ]] || { echo ; echo '[ERROR]  Missing argument: --disk-id.'       ; return 3 ; }
 
     local       MBR_PATH=${MKTSUSBSTICK_OPTIONS[--mbr-path]}
     local  BOOT_DIR_PATH=${MKTSUSBSTICK_OPTIONS[--boot-dir-path]}
@@ -46,18 +46,18 @@ function mktsusbstick # --ts-repo <repo_path> --disk-id <disk_id> --new-volume-n
     local PARTITION_NAME=${MKTSUSBSTICK_OPTIONS[--new-volume-name]:-'THINSTATION'}
 
     local DEV_DISK_PATH="/dev/disk/by-id/${DISK_ID}"
-    [[ -e "${DEV_DISK_PATH}" ]] || { echo ; echo '[ERROR]  Specified disk not found.  Check that your destination disk or USB stick is connected.' ; return 10 }
+    [[ -e "${DEV_DISK_PATH}" ]] || { echo ; echo '[ERROR]  Specified disk not found.  Check that your destination disk or USB stick is connected.' ; return 10 ; }
     
     local DEV_SD_PATH=$(readlink -f "${DEV_DISK_PATH}")
-    [[ -e "${DEV_SD_PATH}" ]]   || { echo ; echo '[ERROR]  Disk device not found.' ; return 20 }
+    [[ -e "${DEV_SD_PATH}" ]]   || { echo ; echo '[ERROR]  Disk device not found.' ; return 20 ; }
     
-    [[ -e "${MBR_PATH}" ]]      || { echo ; echo '[ERROR]  mbr.bin not found at path specified after --mbr-path.' ; return 30 }
-    [[ -d "${BOOT_DIR_PATH}" ]] || { echo ; echo '[ERROR]  Boot files directory specified after --boot-dir-path does not exist.' ; return 40 }
+    [[ -e "${MBR_PATH}" ]]      || { echo ; echo '[ERROR]  mbr.bin not found at path specified after --mbr-path.' ; return 30 ; }
+    [[ -d "${BOOT_DIR_PATH}" ]] || { echo ; echo '[ERROR]  Boot files directory specified after --boot-dir-path does not exist.' ; return 40 ; }
 
     local TEMP_MOUNT_POINT="/mnt/${PARTITION_NAME}"
     local DEV_PARTITION_PATH="${DEV_SD_PATH}1"
     
-    umount -v ${DEV_SD_PATH}?* || true ; sync
+    umount ${DEV_SD_PATH}?* || true ; sync
     
     dd        if=/dev/zero           of=${DEV_SD_PATH}  bs=1M count=2
     parted    --script                 "${DEV_SD_PATH}" mklabel msdos               && sync
@@ -72,15 +72,14 @@ function mktsusbstick # --ts-repo <repo_path> --disk-id <disk_id> --new-volume-n
 
     cp -v -R ${BOOT_DIR_PATH} ${TEMP_MOUNT_POINT}/.
 
-    umount -v ${DEV_SD_PATH}?* && sync
-    eject     ${DEV_SD_PATH} || true
+    umount -v ${DEV_SD_PATH}?* || true && sync
     rm -rf    ${TEMP_MOUNT_POINT}
 
     echo ; echo "[INFO]  ThinStation USB Stick preparation complete.  Please remove the USB drive."
 }
 
 echo
-echo "[INFO]  Function loaded.  Usage: # mktsusbstick --ts-repo <repo_path> --disk-id <disk_id> --new-volume-name [volume_name]"
+echo '[INFO]  Function loaded.  Usage example: # mktsusbstick --mbr-path /usr/lib/syslinux/mbr/mbr.bin --boot-dir-path /media/ubuntu/THINSTATION/boot --disk-id usb-SanDisk_Ultra_USB_3.0_UUID-0:0'
 echo
 
 
